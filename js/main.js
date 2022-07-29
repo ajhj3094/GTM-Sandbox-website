9ae3c809f298534aa3bd90de4e1afb6c48f95ca4
INTECH\justin.yu <justin.yu@ihwic.com>
2022-07-29 14:38:03 +0800

    feat: add google Noto-emoji

--- js/main.js
+++ js/main.js
@@ -2,5 +2,19 @@ const vue = new Vue({
   el: '#main',
   data: {
     
-  }
-})
+  },
+  created () {
+    document.addEventListener("click", function(event) {
+      // console.log(event.target.tagName)
+      if (event.target.tagName === "A") {
+        gtag ("event", "link_click", {
+          "link_text" : event.target.innerText,
+          "link_url" : event.target.href,
+          "link_id" : event.target.id,
+          "link_classes" : event.target.className,
+          "link_value" : 500
+        });
+      };
+    });
+  },
+});
